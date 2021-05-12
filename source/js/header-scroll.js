8c776568b96b2acae4d5ea4d5413a3c0766757ec
chazyspoller <chazyspoller@gmail.com>
2021-05-12 23:04:47 +0300

    Изменение разметки, стилей, расположения при автоматизации сборки

--- source/js/header-scroll.js
+++ source/js/header-scroll.js
@@ -1,12 +1,15 @@
 var pageHeader = document.querySelector('.page-header');
 var navLogo = document.querySelector('.main-nav__logo');
+var navMenu = document.querySelector('.main-nav__menu');
 
 window.addEventListener('scroll', function() {
   if (window.pageYOffset >= (pageHeader.offsetHeight - 2)) {
     pageHeader.classList.add('page-header--scroll');
-    navLogo.classList.add('main-nav__logo--dark')
+      navLogo.classList.add('main-nav__logo--dark')
   } else {
     pageHeader.classList.remove('page-header--scroll');
-    navLogo.classList.remove('main-nav__logo--dark')
+    if (navMenu.classList.contains('main-nav__menu--closed')) {
+      navLogo.classList.remove('main-nav__logo--dark')
+    }
   }
 });
--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -1,21 +1,24 @@
-var navMain = document.querySelector('.main-nav__menu');
+var navMenu = document.querySelector('.main-nav__menu');
 var navLogo = document.querySelector('.main-nav__logo');
 var navToggle = document.querySelector('.main-nav__toggle');
+var headerMain = document.querySelector('.main-nav');
 //var body = document.querySelector('.page__body');
 
-navMain.classList.remove('main-nav__menu--nojs');
+navMenu.classList.remove('main-nav__menu--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav__menu--closed')) {
-    navMain.classList.remove('main-nav__menu--closed');
-    navMain.classList.add('main-nav__menu--opened');
+  if (navMenu.classList.contains('main-nav__menu--closed')) {
+    navMenu.classList.remove('main-nav__menu--closed');
+    navMenu.classList.add('main-nav__menu--opened');
     navToggle.classList.add('main-nav__toggle--close');
     navLogo.classList.add('main-nav__logo--dark')
+    headerMain.classList.add('main-nav--margin-top-menu');
     //body.classList.add('page__body--lock-scroll');
   } else {
-    navMain.classList.add('main-nav__menu--closed');
-    navMain.classList.remove('main-nav__menu--opened');
+    navMenu.classList.add('main-nav__menu--closed');
+    navMenu.classList.remove('main-nav__menu--opened');
     navToggle.classList.remove('main-nav__toggle--close');
+    headerMain.classList.remove('main-nav--margin-top-menu');
     //body.classList.remove('page__body--lock-scroll');
     if (!pageHeader.classList.contains('page-header--scroll')) {
       navLogo.classList.remove('main-nav__logo--dark')
